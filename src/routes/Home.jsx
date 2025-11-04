908c1b9d1c4ad8688b0270a099c37a34fbeede46
Enzzzzz <contatoenzz@gmail.com>
2025-11-03 22:16:42 -0300

    added animations for section 2

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -7,22 +7,40 @@ function Home() {
   const textRef = useRef(null);
   const [isVisible, setIsVisible] = useState(true);
 
+  const section2Ref = useRef(null);
+  const [isVisible2, setIsVisible2] = useState(false);
+
+  const fadeLeft = isVisible2
+    ? "animate-fade-in-left opacity-100"
+    : "animate-fade-out-right opacity-0";
+
+  const fadeRight = isVisible2
+    ? "animate-fade-in-right opacity-100"
+    : "animate-fade-out-left opacity-0";
+
   useEffect(() => {
+    const textElement = textRef.current;
+    const section2Element = section2Ref.current;
+
     const observer = new IntersectionObserver(
       (entries) => {
         entries.forEach((entry) => {
-          setIsVisible(entry.isIntersecting);
+          if (entry.target === textRef.current) {
+            setIsVisible(entry.isIntersecting);
+          } else if (entry.target === section2Ref.current) {
+            setIsVisible2(entry.isIntersecting);
+          }
         });
       },
       { threshold: 0.3 }
     );
 
-    if (textRef.current) {
-      observer.observe(textRef.current);
-    }
+    if (textElement) observer.observe(textElement);
+    if (section2Element) observer.observe(section2Element);
 
     return () => {
-      if (textRef.current) observer.unobserve(textRef.current);
+      if (textElement) observer.unobserve(textElement);
+      if (section2Element) observer.unobserve(section2Element);
     };
   }, []);
 
@@ -36,7 +54,12 @@ function Home() {
         >
           <img
             src={Logo}
-            className="w-64 sm2:w-44 sm:w-64 md:w-[450px] 2xlg:w-[550px] rounded-4xl drop-shadow-[0_0_20px_#00ff88] hover:drop-shadow-[0_0_35px_#00ffcc] transition-all duration-700"
+            ref={textRef}
+            className={` w-64 sm2:w-44 sm:w-64 md:w-[450px] 2xlg:w-[550px] rounded-4xl drop-shadow-[0_0_20px_#00ff88] hover:drop-shadow-[0_0_35px_#00ffcc] transition-all duration-700 ${
+              isVisible
+                ? "animate-fade-in-right opacity-100"
+                : "animate-fade-out-left opacity-0"
+            }`}
             alt="Logo"
           />
           <div
@@ -44,7 +67,7 @@ function Home() {
             className={`transition-all duration-700 ${
               isVisible
                 ? "animate-fade-in-left opacity-100"
-                : "animate-fade-out-left opacity-0"
+                : "animate-fade-out-right opacity-0"
             }`}
           >
             <h1 className="text-4xl font-bebas text-center m-10 font-extrabold text-transparent bg-clip-text bg-linear-to-r from-green-900 via-emerald-500  to-green-900 sm:text-4xl lg:text-5xl xl:text-7xl animate-gradient">
@@ -62,13 +85,18 @@ function Home() {
             </h1>
           </div>
         </section>
+
         <div className="h-32 bg-gradient-to-b from-[#070f08] to-[#0e300a]"></div>
+
         <section
           id="secondLayer"
           className="section bg-green-gradient-reverse flex flex-col sm2:-space-y-16"
+          ref={section2Ref}
         >
           <div>
-            <h1 className="font-bebas text-text font-bold text-5xl mb-5">
+            <h1
+              className={`font-bebas text-text font-bold text-5xl mb-5 transition-all duration-700 ${fadeLeft}`}
+            >
               Projects
             </h1>
           </div>
@@ -76,10 +104,10 @@ function Home() {
             <div className="w-[300px] h-[300px] rounded-4xl overflow-hidden hover:drop-shadow-[0_0_8px_#145f24] transition-all duration-700">
               <img
                 src={AlmiroWeb}
-                className="w-full h-full object-cover object-center transition-transform duration-500 ease-in-out hover:scale-110 hover:opacity-90 hover:blur-xs"
+                className={`w-full h-full object-cover object-center transition-transform duration-500 ease-in-out hover:scale-110 hover:opacity-90 hover:blur-xs ${fadeRight}`}
               />
             </div>
-            <div className="p-2 group">
+            <div className={`p-2 group ${fadeLeft}`}>
               <h2 className="text-2xl text-center font-bebas text-accent sm:text-4xl lg:text-5xl xl:text-7xl">
                 Almiro Fragoso Advocacia
                 <p className="text-text/80 text-lg text-justify font-bebas">
@@ -96,15 +124,14 @@ function Home() {
                 </p>
                 <div className="text-left mt-2">
                   <a
-                  href="https://www.almirofragosoadvocacia.com.br/"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                  className="text-2xl font-bebas text-emerald-400 hover:text-accent hover:[text-shadow:0_0_10px_rgba(59,130,246,0.9)] transition-all duration-300 inline-block cursor-pointer"
-                >
-                  Ir para o site
-                </a>
+                    href="https://www.almirofragosoadvocacia.com.br/"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="text-2xl font-bebas text-emerald-400 hover:text-accent hover:[text-shadow:0_0_10px_rgba(59,130,246,0.9)] transition-all duration-300 inline-block cursor-pointer"
+                  >
+                    Ir para o site
+                  </a>
                 </div>
-                
               </h2>
             </div>
           </div>
